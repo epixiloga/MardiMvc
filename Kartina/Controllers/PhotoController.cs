3d5068e3b3801dedffd5010e9c579a1eb9cc616b
epixiloga <hafbenmoussa@gmail.com>
2018-12-18 16:50:17 +0100

    MardiMvc commit

---
+++ Kartina/Controllers/PhotoController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Kartina.Controllers
+{
+    public class PhotoController : Controller
+    {
+        // Requete Get
+        public ActionResult Creer()
+        {
+            return this.View();
+        }
+
+        //Requete Post
+        [HttpPost]
+        public ActionResult Creer(FormCollection dictio)
+        {
+            if (dictio["Titre"] != null)
+            {
+                using (Models.KartinaDotNetFrameworkEntities1 context = new Models.KartinaDotNetFrameworkEntities1())
+                {
+                    Models.Photo photo = new Models.Photo();
+                    photo.Title = dictio["Titre"];
+                    photo.IdVendeur = 5;
+                    context.Photo.Add(photo);
+                    context.SaveChanges();
+                }
+
+            }
+            return this.View();
+        }
+
+
+        //[HttpPost]
+        //public ActionResult Creer(Models.Photo photo)
+        //{
+        //        using (Models.KartinaDotNetFrameworkEntities1 context=new Models.KartinaDotNetFrameworkEntities1())
+        //        {
+        //            photo.IdVendeur = 5;
+        //            context.Photo.Add(photo);
+        //            context.SaveChanges();
+        //        }
+
+
+        //    return this.View();
+        //}
+    }
+}
